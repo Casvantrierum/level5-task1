3f985b6fd3403315ed050f0c2cbb9aa498cde59e
Cas <Casvantrierum@gmail.com>
2020-10-14 11:57:47 +0200

    setup db

---
+++ app/src/main/java/com/example/level5_task1/dao/NoteDao.kt
@@ -0,0 +1,22 @@
+package com.example.level5_task1.dao
+
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.level5_task1.model.Note
+
+@Dao
+interface NoteDao {
+
+    @Query("SELECT * FROM NoteTable LIMIT 1")
+    fun getNotepad(): LiveData<Note?>
+
+    @Insert
+    suspend fun insertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+}
---
+++ app/src/main/java/com/example/level5_task1/repository/NoteRepository.kt
@@ -0,0 +1,36 @@
+package com.example.level5_task1.repository
+
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.level5_task1.database.NotepadRoomDatabase
+import com.example.level5_task1.dao.NoteDao
+import com.example.level5_task1.model.Note
+
+public class NoteRepository(context: Context) {
+
+    private var noteDao: NoteDao
+
+    init {
+        val noteRoomDatabase = NotepadRoomDatabase.getDatabase(context)
+        noteDao = noteRoomDatabase!!.noteDao()
+    }
+
+    fun getNotepad(): LiveData<Note?> {
+        return noteDao.getNotepad()
+    }
+
+
+    suspend fun insertNote(note: Note) {
+        noteDao.insertNote(note)
+    }
+
+    suspend fun deleteNote(note: Note) {
+        noteDao.deleteNote(note)
+    }
+
+    suspend fun updateNote(note: Note) {
+        noteDao.updateNote(note)
+    }
+}
